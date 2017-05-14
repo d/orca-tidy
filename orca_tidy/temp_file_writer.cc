0a3dfaad176baeca93fefcc0346f4f41d7e4acb9
Jesse Zhang <sbjesse@gmail.com>
2017-05-13 20:42:28 -0700

    Remove use of deprecated tmpnam

--- orca_tidy/temp_file_writer.cc
+++ orca_tidy/temp_file_writer.cc
@@ -1,21 +1,24 @@
 // Copyright (c) 2017 Jesse Zhang All rights reserved.
 
 #include "orca_tidy/temp_file_writer.h"
+#include <unistd.h>
 #include <array>
 #include <cstdio>
 
 TempFileWriter::TempFileWriter() {
-  std::array<char, L_tmpnam> tempNameBuf{};
-  std::tmpnam(tempNameBuf.data());
+  std::array<char, L_tmpnam> tempNameBuf{P_tmpdir "orca_tidyXXXXXX"};
+  int fd = mkstemp(tempNameBuf.data());
 
   tempFilename_.assign(tempNameBuf.data());
-  ofile_.open(tempFilename_);
+  file_ = fdopen(fd, "w+");
 }
 
 void TempFileWriter::WriteLine(const std::string& line) {
-  ofile_ << line << '\n';
+  fprintf(file_, "%s\n", line.c_str());
 }
 
 const std::string& TempFileWriter::TempFilename() const {
   return tempFilename_;
 }
+
+TempFileWriter::~TempFileWriter() { fclose(file_); }
--- orca_tidy/temp_file_writer.h
+++ orca_tidy/temp_file_writer.h
@@ -3,6 +3,7 @@
 #ifndef ORCA_TIDY_TEMP_FILE_WRITER_H_
 #define ORCA_TIDY_TEMP_FILE_WRITER_H_
 
+#include <cstdio>
 #include <fstream>
 #include <string>
 #include "orca_tidy/writer.h"
@@ -10,9 +11,10 @@
 class TempFileWriter : public Writer {
  private:
   std::string tempFilename_;
-  std::ofstream ofile_;
+  FILE* file_;
 
  public:
+  virtual ~TempFileWriter();
   TempFileWriter();
   void WriteLine(const std::string& line) override;
   const std::string& TempFilename() const;
