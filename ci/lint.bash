0ea16b04655fef2487d005f9bd2e3e497c080fa2
Jesse Zhang <sbjesse@gmail.com>
2017-05-13 21:31:33 -0700

    WIP: clang-tidy

--- ci/lint.bash
+++ ci/lint.bash
@@ -5,15 +5,25 @@ set -e -x -u -o pipefail
 _main() {
     clang_format
     cpplint
+    clang_tidy
 }
 
 cpplint() {
     vendor/google_styleguide/cpplint/cpplint.py --extensions=cc,h orca_tidy/*
 }
 
+our_code() {
+    git ls-files ':!vendor' ':!.*' "$@"
+}
+
+clang_tidy() {
+    : ${CLANG_TIDY:=/usr/local/opt/llvm/bin/clang-tidy}
+    our_code '*.cc' | xargs "${CLANG_TIDY}" -p build
+}
+
 clang_format() {
     : ${CLANG_FORMAT:=clang-format}
-    git ls-files ':!vendor' ':!.*' '*.cc' '*.h' | xargs "${CLANG_FORMAT}" -i
+    our_code '*.cc' '*.h' | xargs "${CLANG_FORMAT}" -i
     git diff-files --exit-code
 }
 
